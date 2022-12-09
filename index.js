c6c99dc91f18f7eea43332c441f593903afccf42
Hubert Jr <hubertjunior254@icloud.com>
2022-12-09 14:44:45 +0300

    work done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
 
-/* 
+    // iterate over the array of numbers
+    for (let i = 0; i < array.length; i++) {
+      // for the current number, identify a complementary number that adds to our target
+      // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+      const complement = target - array[i];
+      // iterate over the remaining numbers in the array
+      for (let j = i + 1; j < array.length; j++) {
+        // check if any of the remaining numbers is the complement
+        // if so, return true
+        if (array[j] === complement) return true;
+      }
+    }
+    // if we reach the end of the array, return false
+    return false;
+
+  }
+  
+  /* 
   Write the Big O time complexity of your function here
+  // for (let i = 0; i < array.length; i++) {
+  //   // n steps (depending on the length of the input array)
+  //   const complement = target - array[i];
+  //   for (let j = i + 1; j < array.length; j++) {
+  //     // n * n steps (nested loop!)
+  //     if (array[j] === complement) return true;
+  //   }
+  // }
+  // // 1 step
+  // return false;
 */
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+    for the current number, identify a complementary number that adds to our target
+    (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    iterate over the remaining numbers in the array
+      check if any of the remaining numbers is the complement
+        if so, return true
+  if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  I'll iterate over the array of numbers. for the current number, identify a complementary number that adds to our target. Then i'll iterate over the remaining numbers in the array and check if any of the remaining numbers matches the complement. If the remaining numbers matches the complement number, it returns true, and if they do not match it will return false
 */
 
 // You can run `node index.js` to view these console logs
